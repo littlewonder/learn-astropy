4976888a2e87a9b60974677c3fe46e1e4fd57fa2
Abhishek Sharma <hello.abhishek@outlook.com>
2018-04-17 23:04:45 +0530

    documentation search working

--- app.js
+++ app.js
@@ -1,29 +1,48 @@
-var requestDocs = function (query) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            console.log(xhr.responseText);
-        }
-    }
-    let reqURL = 'https://readthedocs.org/api/v2/docsearch/?q=' + query + '&project=astropy&version=stable&language=en';
-    xhr.open('GET', reqURL, true);
-    xhr.send(null);
+var requestDocs = function (q) {
+    let url = 'https://learn-astropy.herokuapp.com/' + q;
+    console.log(url);
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            let totalCount = (json.results.hits['total']);
+            let bingo = document.getElementById('bingo');
+            bingo.innerHTML = "Found " + totalCount + " page(s) matching the search query";
+            for (i = 0; i < 10; i++) {
+                let projectName = (json.results.hits.hits[i].fields['project'][0]);
+                let cardTitle = (json.results.hits.hits[i].fields['title'][0]);
+                let cardContent = (json.results.hits.hits[i].highlight.content[0]);
+
+                let temp = document.createElement("li");
+                temp.className = 'search-card'
+
+                let title = document.createElement("a");
+                title.innerHTML = cardTitle;
+                title.href = (json.results.hits.hits[i].fields['link']) + '.html';
+
+                let body = document.createElement("p");
+                body.innerHTML = cardContent;
+
+                let about = document.createElement("label");
+                about.innerHTML = 'Documentation • In Project ' + projectName;
+
+                temp.appendChild(title);
+                temp.appendChild(about);
+                temp.appendChild(body);
+                document.getElementById('resultsobtained').appendChild(temp);
+            }
+        })
 };
 
-var requestTuts = function (query) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            console.log(xhr.responseText);
-        }
+window.onload = function () {
+    document.getElementsByClassName('search')[0].addEventListener('submit', function (e) {
+        e.preventDefault();
+        let q = document.getElementById('searchbox').value;
+        window.location.href = 'search.html?q=' + q;
+    })
+    if (window.location.pathname === '/search.html') {
+        let query = window.location.search;
+        requestDocs(query);
     }
-    let reqURL = 'https://readthedocs.org/api/v2/docsearch/?q=' + query + '&project=astropy-tutorials&version=stable&language=en';
-    xhr.open('GET', reqURL, true);
-    xhr.send(null);
 }
-
-document.getElementsByClassName('search')[0].addEventListener('submit', function (e) {
-    e.preventDefault();
-    let q = document.getElementById('searchbox').value;
-    requestDocs(q);
-})
